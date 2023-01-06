437c309ea89069824da28b5284e239a2d6419c3e
humza <ahmadhumza7@gmail.com>
2023-01-06 13:36:16 -0800

    Implemented a data source to be exposed to a repo.

--- database/src/main/java/commanderpepper/helpmechoose/database/HMCListDataSource.kt
+++ database/src/main/java/commanderpepper/helpmechoose/database/HMCListDataSource.kt
@@ -5,17 +5,19 @@ import commanderpepper.helpmechoose.database.model.HMCListsValues
 import kotlinx.coroutines.flow.Flow
 
 interface HMCListDataSource {
-    suspend fun getHMCLists(): Flow<List<HMCLists>>
+    fun getHMCLists(): Flow<List<HMCLists>>
 
     suspend fun saveHMCList(hmcList: HMCLists)
 
     suspend fun deleteHMCList(id: String)
 
-    suspend fun getHMCList(id: String): HMCLists
+    suspend fun getHMCList(id: String): Flow<HMCLists?>
 
     suspend fun insertValue(hmcListsValues: HMCListsValues)
 
-    suspend fun getHMCListsValues(id : String): Flow<List<HMCListsValues>>
+    fun getHMCListsValues(id : String): Flow<List<HMCListsValues>>
 
-    suspend fun getListOfValues(id: String): Flow<List<String>>
+    fun getListOfKeyOneValues(id: String): Flow<List<String>>
+
+    fun getListOfKeyTwoValues(id: String): Flow<List<String>>
 }
--- database/src/main/java/commanderpepper/helpmechoose/database/HMCListDataSourceImpl.kt
+++ database/src/main/java/commanderpepper/helpmechoose/database/HMCListDataSourceImpl.kt
@@ -6,31 +6,35 @@ import commanderpepper.helpmechoose.database.room.HMCListDAO
 import kotlinx.coroutines.flow.Flow
 
 class HMCListDataSourceImpl(private val hmcListDAO: HMCListDAO): HMCListDataSource  {
-    override suspend fun getHMCLists(): Flow<List<HMCLists>> {
-        TODO("Not yet implemented")
+    override fun getHMCLists(): Flow<List<HMCLists>> {
+        return hmcListDAO.getHMCLists()
     }
 
     override suspend fun saveHMCList(hmcList: HMCLists) {
-        TODO("Not yet implemented")
+        hmcListDAO.insertList(hmcList)
     }
 
     override suspend fun deleteHMCList(id: String) {
-        TODO("Not yet implemented")
+        hmcListDAO.deleteHMCListById(id)
     }
 
-    override suspend fun getHMCList(id: String): HMCLists {
-        TODO("Not yet implemented")
+    override suspend fun getHMCList(id: String): Flow<HMCLists?> {
+        return hmcListDAO.getHMCListById(id)
     }
 
     override suspend fun insertValue(hmcListsValues: HMCListsValues) {
-        TODO("Not yet implemented")
+        hmcListDAO.insertValue(hmcListsValues)
     }
 
-    override suspend fun getHMCListsValues(id: String): Flow<List<HMCListsValues>> {
-        TODO("Not yet implemented")
+    override fun getHMCListsValues(id: String): Flow<List<HMCListsValues>> {
+        return hmcListDAO.getHMCListsValues(id)
     }
 
-    override suspend fun getListOfValues(id: String): Flow<List<String>> {
-        TODO("Not yet implemented")
+    override fun getListOfKeyOneValues(id: String): Flow<List<String>> {
+        return hmcListDAO.getListOfKey1(id)
+    }
+
+    override fun getListOfKeyTwoValues(id: String): Flow<List<String>> {
+        return hmcListDAO.getListOfKey2(id)
     }
 }
