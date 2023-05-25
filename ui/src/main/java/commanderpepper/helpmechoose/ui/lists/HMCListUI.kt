70e8340c08861c1862471cd174376c6e36f47dcc
humza <ahmadhumza7@gmail.com>
2023-05-24 20:11:37 -0700

    Added a delete image in HMCListRowUI.

--- ui/src/main/java/commanderpepper/helpmechoose/ui/lists/HMCListUI.kt
+++ ui/src/main/java/commanderpepper/helpmechoose/ui/lists/HMCListUI.kt
@@ -1,13 +1,17 @@
 package commanderpepper.helpmechoose.ui.lists
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import commanderpepper.helpmechoose.ui.R
 
 
 @Composable
@@ -23,9 +27,13 @@ fun HMCListUI(modifier: Modifier = Modifier, hmcList: List<String>, onHMCClick: 
 
 @Composable
 fun HMCListRowUI(modifier: Modifier = Modifier, hmcItem: String, onHMCClick: () -> Unit, onDeleteClick: () -> Unit){
-    Row(modifier = modifier.clickable { onHMCClick() }, horizontalArrangement = Arrangement.SpaceBetween) {
+    Row(
+        modifier = modifier.clickable { onHMCClick() },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
         BasicText(text = hmcItem)
-        BasicText(modifier = Modifier.clickable { onDeleteClick() }, text = "Delete")
+        Image(modifier = Modifier.clickable { onDeleteClick() }, painter = painterResource(id = R.drawable.ic_delete), contentDescription = "Delete HMC List")
     }
 }
 
