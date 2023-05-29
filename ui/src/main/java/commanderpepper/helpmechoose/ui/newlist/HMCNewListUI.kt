2102ebb2b0aa94faabc5cbf298357d2188ac5c48
humza <ahmadhumza7@gmail.com>
2023-05-28 20:48:45 -0700

    Added an alert dialog that will allow users to add a new item to a list.

--- ui/src/main/java/commanderpepper/helpmechoose/ui/newlist/HMCNewListUI.kt
+++ ui/src/main/java/commanderpepper/helpmechoose/ui/newlist/HMCNewListUI.kt
@@ -4,18 +4,47 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import commanderpepper.helpmechoose.ui.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HMCNewListUI(modifier: Modifier = Modifier, onSaveClick: () -> Unit, data: List<String>) {
+fun HMCNewListUI(modifier: Modifier = Modifier, data: List<String>, onSaveClick: () -> Unit) {
+    val showNewItemAlertDialog = remember { mutableStateOf(false) }
+    if(showNewItemAlertDialog.value){
+        AlertDialog(onDismissRequest = { showNewItemAlertDialog.value = false }) {
+            Column() {
+                var newItem : String = ""
+                TextField(value = newItem, onValueChange = { newValue -> newItem = newValue })
+                Button(onClick = {
+                    //TODO: Add new item to whatever list contains data being displayed
+                    showNewItemAlertDialog.value = false
+                }) {
+                    BasicText(text = "Save Item")
+                }
+            }
+        }
+    }
     Column(modifier = modifier) {
         BasicText(text = "Title")
-        Column() {
+        Button(onClick = { showNewItemAlertDialog.value = true }) {
+            BasicText("Add new item")
+        }
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
             data.forEach { 
                 BasicText(text = it)
             }
@@ -28,6 +57,14 @@ fun HMCNewListItemUI(modifier: Modifier = Modifier, newItem: String, editItemOnC
     Row(modifier = modifier) {
         Image(modifier = Modifier.clickable { editItemOnClick() }, painter = painterResource(id = R.drawable.ic_edit), contentDescription = "Edit HMC item")
         BasicText(text = newItem)
+    }
+}
+
+@Preview(device = Devices.PIXEL_3A)
+@Composable
+fun HMCNewListUIPreview(){
+    HMCNewListUI(modifier = Modifier.fillMaxSize(), data = listOf("One", "Two", "Three", "Four")){
+
     }
 }
 
