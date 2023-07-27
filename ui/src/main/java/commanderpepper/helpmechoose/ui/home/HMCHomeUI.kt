b077ac602cd680fd4b45dc55f5a6c6943ab7aeb2
commanderpepper <ahmadhumza7@gmail.com>
2023-07-26 21:14:52 -0700

    Added a button to handle on new list event.

--- ui/src/main/java/commanderpepper/helpmechoose/ui/home/HMCHomeUI.kt
+++ ui/src/main/java/commanderpepper/helpmechoose/ui/home/HMCHomeUI.kt
@@ -1,14 +1,18 @@
 package commanderpepper.helpmechoose.ui.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import commanderpepper.helpmechoose.ui.lists.HMCListUI
 import commanderpepper.helpmechoose.uimodel.HMCItem
 import org.koin.androidx.compose.koinViewModel
@@ -39,18 +43,24 @@ fun HMCHomeUI(
     onDeleteClick: () -> Unit,
     onAddClick: () -> Unit,
 ) {
-    if (hmcList.isEmpty()) {
-        Column(
-            modifier = modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            BasicText(text = "Add a list!")
+    Box(modifier = modifier.fillMaxSize()){
+        if (hmcList.isEmpty()) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                BasicText(text = "Add a list!")
+            }
+        }
+        else {
+            HMCListUI(hmcList = hmcList, onHMCClick = onHMCClick, onDeleteClick = onDeleteClick)
+        }
+        Button(modifier = Modifier.align(alignment = Alignment.BottomEnd).padding(16.dp), onClick = onAddClick){
+            BasicText(text = "New List")
         }
     }
-    else {
-        HMCListUI(hmcList = hmcList, onHMCClick = onHMCClick, onDeleteClick = onDeleteClick)
-    }
+
 }
 
 @Preview
