5f77777760131a25fa4c981424f13d9406c29f9c
commanderpepper <ahmadhumza7@gmail.com>
2023-07-29 21:31:48 -0700

    Added a UI state Sealed Class that for HMCHomeUI

--- ui/src/main/java/commanderpepper/helpmechoose/ui/home/HMCHomeUI.kt
+++ ui/src/main/java/commanderpepper/helpmechoose/ui/home/HMCHomeUI.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -14,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import commanderpepper.helpmechoose.ui.lists.HMCListUI
-import commanderpepper.helpmechoose.uimodel.HMCItem
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -25,10 +25,10 @@ fun HMCHomeUI(
     onDeleteClick: () -> Unit,
     onAddClick: () -> Unit,
 ){
-    val hmcList = hmcHomeViewModel.hmcItemList.collectAsState()
+    val homeUIState = hmcHomeViewModel.hmcItemList.collectAsState()
     HMCHomeUI(
-        modifier = modifier,
-        hmcList = hmcList.value,
+        modifier = modifier.padding(16.dp),
+        homeUIState = homeUIState.value,
         onHMCClick = onHMCClick,
         onDeleteClick = onDeleteClick,
         onAddClick = onAddClick
@@ -38,25 +38,38 @@ fun HMCHomeUI(
 @Composable
 fun HMCHomeUI(
     modifier: Modifier = Modifier,
-    hmcList: List<HMCItem>,
+    homeUIState: HomeUIState,
     onHMCClick: () -> Unit,
     onDeleteClick: () -> Unit,
     onAddClick: () -> Unit,
 ) {
     Box(modifier = modifier.fillMaxSize()){
-        if (hmcList.isEmpty()) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                BasicText(text = "Add a list!")
+        when(homeUIState){
+            HomeUIState.Loading -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+            is HomeUIState.NoData -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    BasicText(text = "Add a list!")
+                }
+            }
+            is HomeUIState.Success -> {
+                HMCListUI(hmcList = homeUIState.hmcItems, onHMCClick = onHMCClick, onDeleteClick = onDeleteClick)
             }
         }
-        else {
-            HMCListUI(hmcList = hmcList, onHMCClick = onHMCClick, onDeleteClick = onDeleteClick)
-        }
-        Button(modifier = Modifier.align(alignment = Alignment.BottomEnd).padding(16.dp), onClick = onAddClick){
+        Button(modifier = Modifier
+            .align(alignment = Alignment.BottomEnd)
+            .padding(16.dp), onClick = onAddClick){
             BasicText(text = "New List")
         }
     }
@@ -66,5 +79,5 @@ fun HMCHomeUI(
 @Preview
 @Composable
 fun HMCHomeUIPreview(){
-    HMCHomeUI(hmcList = emptyList(), onHMCClick = {}, onDeleteClick = {}, onAddClick = {})
+    HMCHomeUI(homeUIState = HomeUIState.Success(emptyList()), onHMCClick = {}, onDeleteClick = {}, onAddClick = {})
 }
--- ui/src/main/java/commanderpepper/helpmechoose/ui/home/HMCHomeViewModel.kt
+++ ui/src/main/java/commanderpepper/helpmechoose/ui/home/HMCHomeViewModel.kt
@@ -9,11 +9,25 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
-class HMCHomeViewModel(hmcListDataSource: HMCListDataSource): ViewModel() {
-    val hmcItemList: StateFlow<List<HMCItem>> = hmcListDataSource.getHMCLists().map {
-        it.map { hmcLists ->
-            HMCItem(id = hmcLists.id, name = hmcLists.name)
+class HMCHomeViewModel(hmcListDataSource: HMCListDataSource) : ViewModel() {
+    val hmcItemList: StateFlow<HomeUIState> = hmcListDataSource.getHMCLists().map { data ->
+        if (data.isNotEmpty())
+            HomeUIState.Success(data.map { hmcLists ->
+                HMCItem(id = hmcLists.id, name = hmcLists.name)
+            })
+        else {
+            HomeUIState.NoData()
         }
-    }.stateIn(scope = viewModelScope, started = SharingStarted.WhileSubscribed(5_000L), initialValue = emptyList())
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000L),
+        initialValue = HomeUIState.Loading
+    )
 
+}
+
+sealed class HomeUIState() {
+    object Loading : HomeUIState()
+    data class Success(val hmcItems: List<HMCItem>) : HomeUIState()
+    data class NoData(val message: String = "Add a list!") : HomeUIState()
 }
